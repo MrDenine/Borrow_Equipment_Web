e99a7999a81a05aa1490d8a76057244a1a8cf809
NineTongnois <panuwat_nine@hotmail.com>
2022-02-26 18:36:22 +0700

    lol

--- routes/signinRoute.js
+++ routes/signinRoute.js
@@ -1,8 +1,60 @@
 const express = require('express');
 const router = express.Router();
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const config = require('../config');
+const encrypt_decrypt_tools = require('../utils/encrypt_decrypt_tools');
+const { response } = require('../app');
+const {accessCookieExist} = require('../middleware/validation_user');
 
-router.get('/', (req, res, next) => {
+router.use(bodyParser.urlencoded({extended : false}));
+router.use(bodyParser.json());
+
+router.use(cookieParser());
+router.get('/',accessCookieExist, (req, res, next) => {
     res.render('signin', { data: 'signin' });
+})
+
+router.post('/PostSignin',function(req,res,next){
+    var username = req.body.Username; 
+    var password = req.body.Password;
+    if(username && password){
+        //call postLogin
+        axios
+        .post(config.servurl + '/Login/PostLogin',{
+            email : username,
+            password : password,
+        })
+        .then(function (response) {
+            if(response.data.status == "Succeed"){
+                //call getUserdata
+                axios
+                .get(config.servurl + '/GetDataUser/'+ response.data.data[0].id)
+                .then(function(response){
+                    //get userdata
+                    userdata = response.data.data[0];
+                    userdata_enc = encrypt_decrypt_tools.encrypt(JSON.stringify(response.data.data[0]));
+                    
+                    // //setCookie
+                    res.cookie('UDT', userdata_enc, config.cookie_options);
+                    res.status(200).send(response.data);
+                    return;
+                })
+                .catch(function(response){
+                    res.status(400).send(response.data);
+                    return;
+                });
+            } else {
+                res.status(200).send(response.data); //echo
+                return;
+            }
+        })
+        .catch(function (error) {
+            res.status(400).send(error); 
+            return;
+        });
+    }
 })
 
 module.exports = router;
