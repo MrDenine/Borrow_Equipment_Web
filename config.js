64d6aa022b7f445470d17f4d1c098b5584350842
THAM <thampk@outlook.com>
2022-03-09 22:53:12 +0700

    [UPDATE]DATE report

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const config = {};
 
 config.port = 8000; //website port
-config.servurl = 'http://localhost:3000'; //service url/port
-// config.weburl = 'http://localhost:8000/';
-config.weburl = 'http://157.230.254.198:8000/';
+config.servurl = 'http://157.230.254.198:3000'; //service url/port
+config.weburl = 'http://localhost:8000/';
+// config.weburl = 'http://157.230.254.198:8000/';
 config.cookie_options = {
     maxAge: 86400 * 1000, // 24 hours
-    httpOnly:true,
+    httpOnly: true,
     secure: false, // cookie must be sent over https / ssl
-    path:"/"
+    path: "/"
 }
 config.logout = {
-    maxAge: 0, 
-    httpOnly:true,
+    maxAge: 0,
+    httpOnly: true,
     secure: false, // cookie must be sent over https / ssl
-    path:"/"
+    path: "/"
 }
 
 module.exports = config;
